9974e6fbc383d87f7eaa1ac426772d47bb01ad5c
Justin Pickens <justin.pickens@gmx.com>
2021-06-12 08:33:02 -0500

    Added test API call with return

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
-
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+var requestUrl = BASE_URL + "Dallas,tx,us" + "&appid=" + API_KEY;
+
+function getApi(requestUrl) {
+
+  fetch(requestUrl).then(function (response) {
+
+      return response.json();
+
+  }).then(function (data) {
+
+    console.log(data);
+    
+  });
+}
+
+getApi(requestUrl);
