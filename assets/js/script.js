68cf334fba7b16a7c9acb486c3be8d9eb436667d
Justin Pickens <justin.pickens@gmx.com>
2021-06-14 00:27:32 -0500

    Added a dynamic history list. When you click on a history item, it re-calls the query for that city, and displays on the app

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,14 @@
 const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 const ICON_BASE_URL = "https://openweathermap.org/img/w/";
 
+// localstorage vars
+var aPreviousSearches = []; // Array to contain searches
+var sCurrentSearch = "";
+
 // Element vars
 var eForecastField = $("#forecastField");
 var eSearchField = $("#searchField");
+var eHistoryField = $("#historyField");
 
 // Fields for current day panel
 var aeTodaysWeather = [ $("#cityField"),
@@ -18,6 +23,10 @@ var aeTodaysWeather = [ $("#cityField"),
 
 // Search Button
 var eSearchButton = $("#searchBtn");
+
+function clearHistoryListItems() {
+    eHistoryField.empty();
+}
 
 // Clear all fields in the Current Day panel
 function clearCurrentDayPanel() {
@@ -58,6 +67,7 @@ function populateForecastCards(oDays) {
 
     // If the list already has cards, clear it
     if (eForecastField.children().length > 0) {
+
         clearForecastListItems();
     }
 
@@ -109,6 +119,35 @@ function populateForecastCards(oDays) {
     }
 }
 
+function populateHistoryList() {
+
+        // If the list already has items, clear it
+        if (eHistoryField.children().length > 0) {
+
+            clearHistoryListItems();
+        }
+
+        // If the user has made a second search
+        if (aPreviousSearches.length > 0) {
+
+            // Iterate through all previous searches
+            for (var index = 0; index < aPreviousSearches.length; index++) {
+
+                // Create elements
+                var eListItem = $("<li>");
+                var eListLink = $("<button>");
+
+                // Set Button attributes
+                eListLink.attr("data-query", aPreviousSearches[index]);
+                eListLink.text(aPreviousSearches[index]);
+
+                // Add the Button to the list
+                eListItem.append(eListLink);
+                eHistoryField.append(eListItem);
+            }
+        }
+}
+
 // Use latitude and longitude from last call to get 5 day forecast data
 function getForecast(lat, lon) {
 
@@ -134,7 +173,6 @@ function getForecast(lat, lon) {
         // Only send the next 5 days to populateForecastCards
         var aFiveDays = data.daily.slice(1, 6); // I tested this parameter set with jsfiddle
         populateForecastCards(aFiveDays);
-        console.log(data);
     });
 }
 
@@ -164,6 +202,24 @@ function getWeather(request) {
 
         if (Number(data.cod) < 300) { // If there's something in data
             aeTodaysWeather[0].text(data.name + ", " + data.sys.country); // City name
+
+            // If this isn't the first search
+            if (sCurrentSearch !== "") {
+
+                // Check for duplicates
+                if (!aPreviousSearches.includes(sCurrentSearch)) {
+
+                    // If no duplicates, add to array
+                    aPreviousSearches.push(sCurrentSearch);
+                }
+            }
+
+            // Set the current search element
+            sCurrentSearch = data.name;
+
+            // Display previous searches
+            populateHistoryList();
+
             getForecast(data.coord.lat, data.coord.lon); // Get 5 day forecast
         }
     });
@@ -171,12 +227,32 @@ function getWeather(request) {
 
 // When the user clicks the "Search" button, use the text in the search field
 // to get the weather from the API call
-eSearchButton.click(function() {
+eSearchButton.click(function () {
 
     // Get the text from the search bar
     var sCityInput = eSearchField.val();
+    eSearchField.val(""); // Clear the search bar
 
     // Construct the request URL
     var sRequest = BASE_URL + "weather?appid=" + API_KEY + "&units=imperial&q=" + sCityInput;
     getWeather(sRequest); // Get data from OWM
+
 });
+
+// When a history list item button is clicked
+eHistoryField.click(function (event) {
+
+    // What was clicked?
+    var eClickedElement = event.target; // ! NOT a jQuery object!
+
+    // Make sure it's a button being clicked
+    if (eClickedElement.tagName === "BUTTON") {
+
+        // Get city name from Button
+        var sPreviousSearch = eClickedElement.getAttribute("data-query");
+
+        // Construct the request URL
+        var sRequest = BASE_URL + "weather?appid=" + API_KEY + "&units=imperial&q=" + sPreviousSearch;
+        getWeather(sRequest); // Get data from OWM
+    }
+})
